17129564ca7ac1b2063976393235d8c2aa2eac46
Huf <bjhufstetler@gmail.com>
2022-03-08 20:16:24 -0800

    commit message

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const port = 8080;
+
+const app = express();
+app.use(cookieParser());
+
+app.get('/login', (req, res) => {
+    var opts = {
+        maxAge: 900000,
+        httpOnly: true
+      };
+      res.cookie('name', req.query.name, opts);
+      res.end();
+});
+
+app.get('/hello', (req, res) => {
+    const name = req.cookies.name;
+    res.status(200).send(`Hello, ${name}`);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
